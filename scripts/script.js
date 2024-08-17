cab2f8a90e749c6df3e299fa3a9fe14a4521e230
Dazai <147618841+Dazaid@users.noreply.github.com>
2024-08-18 00:10:40 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,21 @@
-window.addEventListener('scroll', function() {
+window.addEventListener('load', function () {  //---- problemas de effectos con scroll al cargar la pagina
+
+    window.scrollBy(0, 1);
+
+
+    setTimeout(function () {
+        window.scrollBy(0, -1);
+    }, 50);
+});
+
+
+
+window.addEventListener('scroll', function () {
     // Calcular el desplazamiento de la imagen de fondo
     let offset = window.pageYOffset;
     document.body.style.backgroundPositionY = offset * 0 + "px";
-    document.querySelector('.bkg1').style.backgroundPositionY = offset * 0.5 + "px";
-    document.querySelector('.bkg2').style.backgroundPositionY = offset * 0.03 + "px";
+    document.querySelector('.bkg1').style.transform = 'translateY(' + offset * 0.5 + 'px)';
+    document.querySelector('.bkg2').style.transform = 'translateY(' + offset * 0.03 + 'px)';
 });
 
 
@@ -68,3 +80,43 @@ function autoMoveCarousel2() {
 setInterval(autoMoveCarousel2, 5100); // Cambia de imagen cada 3 segundos
 
 updateCarousel2();
+
+
+
+
+// ---------------  Arms header effect  -----------------
+
+var btlHand = document.getElementById('btlHand');
+var cupHand = document.getElementById('cupHand');
+
+// hidden arms effect   //---- problemas de verificar varias veces
+var actionExecuted = false;
+window.addEventListener('scroll', function () {
+
+    scrollPosition = window.scrollY;
+
+    if (!actionExecuted && scrollPosition < 400) {
+
+        btlHand.setAttribute("class", "fixed top-28 right-0 w-[45vw] overflow-hidden z-1 animate__fadeInRight animate__animated animate__slow opacity-1");
+        cupHand.setAttribute("class", "fixed top-80 left-0 w-[45vw] overflow-hidden z-50 animate__fadeInLeft animate__animated animate__slow opacity-1");
+        setTimeout(function () {
+            cupHand.classList.add("animation-lefthand");
+        }, 2000);
+
+        actionExecuted = true;
+    }
+
+    if (actionExecuted && scrollPosition >= 400) {
+        btlHand.setAttribute("class", "fixed top-28 right-0 w-[45vw] overflow-hidden z-1 animate__rollOut animate__animated animate__fast opacity-0");
+        cupHand.setAttribute("class", "fixed top-80 left-0 w-[45vw] overflow-hidden z-1 animate__fadeOutBottomLeft animate__animated animate__fast opacity-0");
+
+        actionExecuted = false;
+    }
+
+    if (actionExecuted && scrollPosition >= 640) {
+        cupHand.classList.add("hidden");
+    }
+
+});
+
+
